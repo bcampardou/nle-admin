56475ce289af9607f4d407b2eb5dacf73cd4870a
Béranger Campardou <b.campardou@gmail.com>
2016-01-11 21:24:46 +0100

    Initial commit

---
+++ src/node-log-admin/ViewModels/Manage/FactorViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace node_log_admin.ViewModels.Manage
+{
+    public class FactorViewModel
+    {
+        public string Purpose { get; set; }
+    }
+}
