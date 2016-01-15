a1125e64e724a106c091ad7046a755706195e6ce
Béranger Campardou <b.campardou@gmail.com>
2016-01-16 00:07:40 +0100

    dynamically add applications into the list (side bar menu)

--- src/node-log-admin/wwwroot/js/site.js
+++ src/node-log-admin/wwwroot/js/site.js
@@ -1,1 +1,13 @@
 ﻿// Write your Javascript code.
+
+$(document).ready(function () {
+    $.ajax({
+        url: "http://localhost:3200/adm/keys?key=ce411675-1824-4a02-98fc-c68ce6705ea7",
+        method: 'GET',
+        dataType: 'jsonp'
+    }).done(function (data) {
+        for (var i in data) {
+            $('#hostList').append($('<li><a title="Log" href="/Log"><i class="fa fa-circle-o"></i> ' + data[i] + '</a></li>'));
+        }
+    });
+});
