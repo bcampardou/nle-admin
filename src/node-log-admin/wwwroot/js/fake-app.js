2ee6ee8deb19f7e24e0ae0f2f2469a7567c6b4bd
Béranger Campardou <b.campardou@gmail.com>
2016-01-26 11:56:37 +0100

    Fix bug on cors requests

--- src/node-log-admin/wwwroot/js/fake-app.js
+++ src/node-log-admin/wwwroot/js/fake-app.js
@@ -20,18 +20,30 @@
 });
 
 var fakeMessage = function (level) {
-    return {
+    var data = {
         date: moment().format(),
         message: lipsumjs.generateSentence(10, 30),
         level: level,
         stacktrace: ''
     };
+    if (level === 'error')
+        data.stacktrace = "at System.IO.__Error.WinIOError(Int32 errorCode, String maybeFullPath) \
+    at System.IO.FileStream.Init(String path, FileMode mode, FileAccess access, Int32 rights, Boolean useRights, FileShare share, Int32 bufferSize, FileOptions options, SECURITY_ATTRIBUTES secAttrs, String msgPath, Boolean bFromProxy) \
+    at System.IO.FileStream..ctor(String path, FileMode mode, FileAccess access, FileShare share, Int32 bufferSize, FileOptions options) \
+    at System.IO.StreamReader..ctor(String path, Encoding encoding, Boolean detectEncodingFromByteOrderMarks, Int32 bufferSize) \
+    at System.IO.StreamReader..ctor(String path) \
+    at LVKWinFormsSandbox.MainForm.button1_Click(Object sender, EventArgs e) in C:\Dev\VS.NET\Gatsoft\LVKWinFormsSandbox\MainForm.cs:line 36";
+
+    return data;
 };
 
-var putLog = function(sentData) {
+var putLog = function (sentData) {
+    var url = 'http://localhost:3200/log/nodelog-admin?_method=put&key=ce411675-1824-4a02-98fc-c68ce6705ea7';
+
     $.ajax({
-        url: 'http://localhost:3200/log/nodelog-admin?_method=put&key=ce411675-1824-4a02-98fc-c68ce6705ea7',
+        url: url,
         data: sentData,
+        crossDomain: true,
         dataType: 'json',
         method: 'POST'
     }).success(function (data) {
@@ -40,3 +52,4 @@ var putLog = function(sentData) {
         window.alert("error : " + errorThrown);
     });
 };
+
